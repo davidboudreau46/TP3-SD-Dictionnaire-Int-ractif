65f74da690adb22be7ca8544cf1b909c344ffc49
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2015-12-01 09:51:15 -0500

    Ajout CLR

---
+++ InteractiveDictionaryCLR/InteractiveDictionary/InteractiveDictionary.h
@@ -0,0 +1,13 @@
+// InteractiveDictionary.h
+
+#pragma once
+
+using namespace System;
+
+namespace InteractiveDictionary {
+
+	public ref class Class1
+	{
+		// TODO: ajoutez ici vos méthodes pour cette classe.
+	};
+}
